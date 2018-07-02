ebd8d67d54ddcd30908b8d4c080b949284b1ac02
StevenCode <codesteven@163.com>
2018-07-03 01:27:46 +0800

    网络下载完成

--- src/main/java/org/steven/zhihu/Constant.java
+++ src/main/java/org/steven/zhihu/Constant.java
@@ -1,5 +1,16 @@
 package org.steven.zhihu;
 
+import org.springframework.context.ApplicationContext;
+import org.steven.zhihu.model.Activities;
+import org.steven.zhihu.model.Table;
+
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Constant {
     public static final int HTTP_STAUTS_OK = 200;
+    public static final BlockingQueue<String> urlQueue = new LinkedBlockingQueue<String>();
+    public static final BlockingQueue<Activities> tableQueue = new LinkedBlockingQueue<Activities>();
+    public static  ApplicationContext context;
 }
